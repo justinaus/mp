799921fefe2520b2c34e0d7153aac0521d70a398
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-11-22 14:09:20 +0900

    Add lint + prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 // import { Inter } from 'next/font/google'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -12,9 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        Hello
-      </main>
+      <main>Hello</main>
     </>
-  )
+  );
 }
