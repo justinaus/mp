949d44476310778dc0e8ebcd8092492604202ef7
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-11-30 20:11:40 +0900

    Update test

--- src/components/explore/ExploreNaverMapContent.tsx
+++ src/components/explore/ExploreNaverMapContent.tsx
@@ -1,6 +1,7 @@
 import { Box, Stack } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import queryString from 'query-string';
+import { useMap } from 'react-naver-maps';
 import { useRecoilValue } from 'recoil';
 
 import { NaverAllSearchResponse } from '@/pages/api/naver/search/allSearch';
@@ -14,15 +15,39 @@ import ExploreRestaurantsDrawer from './ExploreRestaurantsDrawer';
 export default function ExploreNaverMapContent() {
   const mapCenter = useRecoilValue(exploreMapCenterState);
 
+  const map = useMap();
+
+  console.log(map);
+
+  // center도 지도에 따라서??
+  // 클릭 시, params state를 만들고 그걸 변경.
+
+  // boundary: 127.01109044483923;37.28124946324532;127.19148226485783;37.41253785846415
+
   const { data } = useQuery<NaverAllSearchResponse>({
     queryKey: ['naver/search/allSearch', mapCenter.lng, mapCenter.lat],
     queryFn: () => {
+      let boundary: string | null = null;
+
+      if (map) {
+        const bounds = map.getBounds();
+
+        const arr = [
+          bounds.minX(),
+          bounds.minY(),
+          bounds.maxX(),
+          bounds.maxY(),
+        ];
+
+        boundary = arr.join(';');
+      }
+
       const obj = {
         query: '음식점',
         type: 'all',
         searchCoord: `${mapCenter.lng};${mapCenter.lat}`,
         // page: '2',
-        // boundary
+        boundary: boundary,
       };
 
       return fetch(
